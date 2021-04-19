8ad2b8068b6363e11f654a5d4d917ce8e34138db
Zerkia <lordzerkia@gmail.com>
2021-04-19 11:17:55 +0200

    new objects, makeMenuKort problem

--- Main.java
+++ Main.java
@@ -1,11 +1,20 @@
 package MariosPizza;
 
 public class Main {
-  void run(){
 
+  MenuKort menu = new MenuKort();
+
+  public void displayMenuKort(){
+    for (int i = 0; i < menu.menuKort.size(); i++) {
+      System.out.println(menu.menuKort.get(i));
+    }
+  }
+
+  void run(){
+    displayMenuKort();
   }
 
   public static void main(String[] args) {
-
+    new Main().run();
   }
 }
---
+++ MenuKort.java
@@ -0,0 +1,13 @@
+package MariosPizza;
+
+import java.util.ArrayList;
+
+public class MenuKort {
+  ArrayList<Pizza> menuKort = new ArrayList<>();
+
+  //Pizza Vesuvio = new Pizza(1, "Vesuvio");
+
+  public void makeMenuKort(){
+    menuKort.add(new Pizza(1, "Vesuvio"));
+  }
+}
